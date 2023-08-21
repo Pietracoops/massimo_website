4f8a0721ea8a03f8bc464209f492161ffef2990d
roamingterra <danielballerini1@gmail.com>
2023-08-21 17:07:29 -0400

    Create functions that receive repos and place them
    I created a file called github-repo-project-cards.js. This file contains
    three functions; a function that makes an API call to get all repos from
    a particular github user, a function that takes that data and creates
    project cards to be appended to the dom, and a controller function that
    controls the flow of the first two functions.
    
    I realized, however, that the injected cards are not being styled like
    the hard coded cards are. After doing some digging, it seems as though
    the scripts found at the end of the index.html file are jquery plugins
    that control the styling of the cards. I want to separate the logic of
    these jquery scripts, create a new controller function found in a
    separate file, and have that file control the flow of all of these
    functions. I believe that the jquery scripts in charge of styling those
    cards are not being called after the new cards are injected into that
    space.

---
+++ js/github-repo-project-cards.js
@@ -0,0 +1,135 @@
+// SELECTED GITHUB ACCOUNT FROM THE GITHUB API
+const url = "https://api.github.com/users/pietracoops/repos";
+
+// THIS FUNCTION FETCHES THE REPOS FROM THE ACCOUNT,AND RETURNS THE NECESSARY DATA FROM EACH PROJECT, ORGANIZED BY LANGUAGE
+const getGitHubRepos = async function () {
+  try {
+    // Fetch repo data
+    const data = await fetch(url);
+    const dataJSON = await data.json();
+
+    // Add languages of each repo into array of languages
+    const languages = [];
+
+    await dataJSON.map((repo) => {
+      languages.push(repo.language);
+    });
+
+    // Reduce array of languages to contain only unique values
+    const uniqueLanguages = languages.filter((value, index, array) => {
+      return array.indexOf(value) === index;
+    });
+
+    // Reorder array of unique languages based on the number of projects that use said language
+    const numOfOccurrencesOfLanguages = {};
+
+    uniqueLanguages.map((language) => {
+      numOfOccurrencesOfLanguages[language] = 0;
+    });
+
+    dataJSON.map((repo) => {
+      numOfOccurrencesOfLanguages[repo.language] += 1;
+    });
+
+    const sortedUniqueLanguages = uniqueLanguages.sort((a, b) => {
+      const projectsA = numOfOccurrencesOfLanguages[a] || 0; // Default to 0 if language not found
+      const projectsB = numOfOccurrencesOfLanguages[b] || 0; // Default to 0 if language not found
+
+      return projectsB - projectsA; // Sort in descending order of project count
+    });
+
+    // Create a new array of objects
+    const projectsByLanguage = [];
+
+    // Organize the objects within the array by language
+    for (let i = 0; i < sortedUniqueLanguages.length; i++) {
+      projectsByLanguage.push({
+        language: sortedUniqueLanguages[i] || "other", // Some repos from the api call may have languages that equal to null
+        projects: [],
+      });
+
+      // Place all projects into array of objects, organized by language
+      dataJSON.map((repo) => {
+        if (
+          repo.language === projectsByLanguage[i].language ||
+          (projectsByLanguage[i].language === "other" && repo.language === null) // Include the case of repos that have their languages equal to null
+        ) {
+          projectsByLanguage[i].projects.push({
+            name: repo.name,
+            description: repo.description,
+            url: repo.html_url,
+          });
+        }
+      });
+    }
+    return projectsByLanguage;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// THIS FUNCTION THAT CREATES PROJECT CARDS AND APPENDS THEM TO THE DOM OF THE WEBSITE
+const projectCardsAppendToDom = function (projectsByLanguage) {
+  console.log(projectsByLanguage);
+
+  // Declare elements in html that this function will interact with
+  const portfolioNav = document.querySelector(".Portfolio-nav");
+  const portfolioContainer = document.querySelector(".portfolioContainer");
+
+  for (let i = 0; i < projectsByLanguage.length; i++) {
+    // Create navigation button for language
+    const navButton = document.createElement("li");
+    const navButtonAnchorTag = document.createElement("a");
+
+    // Assign classes, attributes and text
+    navButtonAnchorTag.setAttribute("href", "#");
+    navButtonAnchorTag.setAttribute(
+      "data-filter",
+      `.${projectsByLanguage[i].language.replace(/\s+/g, "-")}`
+    );
+    navButtonAnchorTag.textContent = projectsByLanguage[i].language;
+
+    // Append navigation button to dom
+    portfolioNav.appendChild(navButton);
+    navButton.appendChild(navButtonAnchorTag);
+
+    for (let j = 0; j < projectsByLanguage[i].projects.length; j++) {
+      // Create elements for cards
+      const cardContainer = document.createElement("div");
+      const linkToRepo = document.createElement("a");
+      const repoImg = document.createElement("img");
+      const projectName = document.createElement("h3");
+      const projectDescription = document.createElement("p");
+
+      // Assign classes and text content
+      cardContainer.classList.add(
+        "Portfolio-box",
+        projectsByLanguage[i].language.replace(/\s+/g, "-"),
+        "isotope-item"
+      );
+      //   cardContainer.style.position = "absolute";
+      //   cardContainer.style.left = "0px";
+      //   cardContainer.style.top = "0px";
+      //   cardContainer.style.transform = "translate3d(15px, 380px, 0px)";
+      linkToRepo.setAttribute("href", projectsByLanguage[i].projects[j].url);
+      repoImg.setAttribute("src", "img/Portfolio-pic1.jpg");
+      projectName.textContent = projectsByLanguage[i].projects[j].name;
+      projectDescription.textContent =
+        projectsByLanguage[i].projects[j].description;
+
+      // Append card to dom
+      portfolioContainer.appendChild(cardContainer);
+      cardContainer.appendChild(linkToRepo);
+      linkToRepo.appendChild(repoImg);
+      cardContainer.appendChild(projectName);
+      cardContainer.appendChild(projectDescription);
+    }
+  }
+};
+
+// IMMEDIATELY INVOKED FUNCTION THAT CONTROLS THE FLOW OF THE DATA FETCHING AND THE APPENDING OF THE CARDS TO THE DOM
+(async function controller() {
+  const retrievedData = getGitHubRepos();
+  console.log(await retrievedData);
+  projectCardsAppendToDom(await retrievedData);
+})();
